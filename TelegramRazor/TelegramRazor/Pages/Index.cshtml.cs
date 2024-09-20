a4160e8b431f561161e1b517b4bb049af4725099
Karsten Jeppesen <kaje@ucn.dk>
2024-09-20 07:54:32 +0200

    TelegramRazor: Added structured info

--- TelegramRazor/TelegramRazor/Pages/Index.cshtml.cs
+++ TelegramRazor/TelegramRazor/Pages/Index.cshtml.cs
@@ -16,11 +16,17 @@ namespace TelegramRazor.Pages
             _logger = logger;
         }
 
+        /// <summary>
+        /// OnGet is called when the browser issues a GET /
+        /// </summary>
         public void OnGet()
         {
             myTest = ["Line1", "Line2", "Line3", "Line4"];
         }
 
+        /// <summary>
+        /// OnPost is called when the browser issues a POST /
+        /// </summary>
         public void OnPost()
         {
         }
